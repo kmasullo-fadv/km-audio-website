2475a5f670706d006799fbcac22ab45471975602
SoundSlab <k_masullo@outlook.com>
2021-01-07 00:59:27 -0500

    contact finished

--- src/Contact/Contact.js
+++ src/Contact/Contact.js
@@ -8,13 +8,52 @@ export default class Contact extends Component {
             <div className="contactDiv">
                     <div className="contactInner">
                         <form className="contactForm" action="https://formspree.io/xvowbnak" method="post">
+                            <h2 className="center">Quote Request Form</h2>
+                            <p className="center">
+                                Fill this form out if you would like to request a quote for your project. You 
+                                may also send general inquiries to K_Masullo@outlook.com
+                            </p>
                             <label htmlFor="user-name">Name:</label>
-                            <input id="user-name" type="text" name="name" required />
+                            <input className="hoverBorder" id="user-name" type="text" name="name" required />
 
-                            <label htmlFor="user-email">Email:</label>
-                            <input id="user-email" type="text" name="email" required />
+                            <label htmlFor="userEmail">Email:</label>
+                            <input className="hoverBorder" id="user-email" type="text" name="email" required />
 
-                            <label htmlFor="user-message">Message:</label>
+                            <label htmlFor="bandName">Band/Project Name:</label>
+                            <input className="hoverBorder" id="bandName" type="text" name="bandName" required />
+
+                            <label htmlFor="numberOfSongs">Number of Songs:</label>
+                            <input className="hoverBorder" id="numberOfSongs" type="text" name="numberOfSongs" required />
+
+                            <label htmlFor="budget">Budget:</label>
+                            <input className="hoverBorder" id="budget" type="text" name="budget" required />
+
+                            <label htmlFor="startDate">Desired Start Date:</label>
+                            <input className="hoverBorder" id="startDate" type="text" name="startDate" required />
+
+                            <p>Desired Services:</p>
+
+                            <label htmlFor="mixingCheckbox">
+                                <input type="checkbox" id="mixingCheckbox" name="mixingCheckbox" value="Yes" />
+                                Mixing
+                            </label>
+
+                            <label htmlFor="quantizingCheckbox">
+                                <input type="checkbox" id="quantizingCheckbox" name="quantizingCheckbox" value="Yes" />
+                                Quantizing
+                            </label>
+                
+                            <label htmlFor="vocalTuningCheckbox">
+                                <input type="checkbox" id="vocalTuningCheckbox" name="vocalTuning" value="Yes" />
+                               Vocal Tuning
+                            </label>
+                            
+                            <label htmlFor="songwritingArrangementsCheckbox">
+                                <input type="checkbox" id="songwritingArrangementsCheckbox" name="songwritingArrangements" value="Yes" />
+                                Songwriting/Arrangements
+                            </label>
+
+                            <label id="messageLabel" htmlFor="user-message">Message:</label>
                             <textarea id="user-message" name="message" required></textarea>
                             <button className="formButton" type="submit">Submit</button>
                             <p>You may also contact me directly at kameronmasullo@gmail.com</p>
